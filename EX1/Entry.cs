2832028933819d45e38d9925aff858128165dd16
loutinier <1361439473@qq.com>
2020-05-16 22:05:52 +0800

    cr

--- EX1/Entry.cs
+++ EX1/Entry.cs
@@ -13,15 +13,22 @@ namespace EX1
         static void Main()
         {
             // interface
-            string[] filePaths = new string[]{ 
+            string[] filePaths = new string[]{
                 @"C:\Users\13614\OneDrive\4\数据挖掘\实验\EX1\T1014D1K.dat",
                 @"C:\Users\13614\OneDrive\4\数据挖掘\实验\EX1\T1014D10K.dat",
                 @"C:\Users\13614\OneDrive\4\数据挖掘\实验\EX1\T1014D50K.dat",
                 @"C:\Users\13614\OneDrive\4\数据挖掘\实验\EX1\T1014D100K.dat",
             };
-            int[] minSup = new int[] {  6,600, 800, 1000 };
-            int useMinSup = minSup[0];
-            string inPath = filePaths[0];
+            int useK;
+            int[][] minSup = new int[][] {
+                new int[]{6,8,10 } ,
+                new int[]{60,80,100},
+                new int[]{300,400,500},
+                new int[]{600,800,1000},
+            };
+            useK = 0;
+            int useMinSup = minSup[useK][1];
+            string inPath = filePaths[useK];
 
             // 从文件读入数据，写到内存里
             DataHelper.SetData(inPath);
@@ -36,12 +43,18 @@ namespace EX1
             // 设置最小支持度
             Apriori.minSup = useMinSup;
             // 求得频繁项集
+            DateTime time1 = DateTime.Now;
             List<Apriori> allSets = Apriori.GetAllSets();
+            DateTime time2 = DateTime.Now;
+            string a = "花费时间：" + Environment.NewLine +
+                "分钟:" + (time2 - time1).TotalMinutes.ToString() + Environment.NewLine +
+                "秒：" + (time2 - time1).TotalSeconds.ToString() + Environment.NewLine;
             // 写入文件
             string aprioriOutPath = inPath.Substring(0, inPath.LastIndexOf(@".")) + "_minsup_" + useMinSup.ToString() + @".txt";
             DataHelper.WriteToFile(aprioriOutPath, allSets);
             // 展示支持度最高的频繁项集
             Apriori.ShowMaxSup(allSets);
+            Console.Write(a);
         }
     }
 }
